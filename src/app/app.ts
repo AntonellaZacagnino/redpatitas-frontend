cec866c9aedde699d139ad18e48e694c141ac42c
ivan10905@hotmail.com <ivan10905@hotmail.com>
2025-11-08 14:07:33 -0300

    estructura base Angular y configuración inicial

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Navbar, Header, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('RedPatitas');
+}
